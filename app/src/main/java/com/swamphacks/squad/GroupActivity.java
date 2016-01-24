e49c264bc115819cb055c8a1133f2a9773d4a49a
manav95 <manavbiodutta@yahoo.com>
2016-01-23 20:40:25 -0500

    kofkfokfk:

---
+++ app/src/main/java/com/swamphacks/squad/GroupActivity.java
@@ -0,0 +1,14 @@
+package com.swamphacks.squad;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class GroupActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+    }
+
+}
