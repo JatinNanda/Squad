5902f49cd4d3e72b01f2d138cafbbad13a742c1a
anott3 <anott3@gatech.edu>
2016-01-24 07:58:24 -0500

    restore transition with Group Activity.

--- app/src/main/java/com/swamphacks/squad/GroupActivity.java
+++ app/src/main/java/com/swamphacks/squad/GroupActivity.java
@@ -8,6 +8,10 @@ public class GroupActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+        Slide sliding = new Slide();
+        sliding.setDuration((long) 15000.0);
+        getWindow().setEnterTransition(sliding);
         setContentView(R.layout.activity_group);
     }
 
