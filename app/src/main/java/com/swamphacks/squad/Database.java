49eab693b413560879664bd485b85b4c95c5b6dd
manav95 <manavbiodutta@yahoo.com>
2016-01-24 03:12:39 -0500

    rkgrrokrk:

--- app/src/main/java/com/swamphacks/squad/Database.java
+++ app/src/main/java/com/swamphacks/squad/Database.java
@@ -4,20 +4,16 @@ package com.swamphacks.squad;
 import com.firebase.client.Firebase;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 import android.content.Context;
 
 public class Database {
     private Firebase myFirebaseRef;
     private User currUser;
-    private HashMap<String, List<String>> squads;
-    private HashMap<String, Event> events;
-    private HashMap<String, List<String>> groups;
     public Database(Context context) {
         /* Create the Firebase ref that is used for all authentication with Firebase */
         myFirebaseRef = new Firebase(context.getResources().getString(R.string.firebase_url));
-        squads = new HashMap<>();
-        events = new HashMap<>();
     }
     public void setCurrUser(User user) {
         this.currUser = user;
@@ -25,11 +21,14 @@ public class Database {
     public User getCurrUser() {
         return this.currUser;
     }
-    public void formSquad(String name, List<Friend> friends) {
-           writeDatabase("squads", name, friends);
+    public void updateSquad(Squad squad) {
+           writeDatabase("squads", squad.getName(), squad.getFriends());
     }
-    public void formEvent(String name, Event event) {
-           writeDatabase("events", name, event.getFriends());
+    public void updateEvent(Event event) {
+           writeDatabase("events", event.getName(), event.getFriends());
+    }
+    public void storeAffiliation(User user, Squad squad) {
+
     }
     public void writeDatabase(String child, String key, Object value) {
         myFirebaseRef.child(this.currUser.getUsername()).child(child).child(key).setValue(value);
--- app/src/main/java/com/swamphacks/squad/Friend.java
+++
@@ -1,7 +0,0 @@
-package com.swamphacks.squad;
-
-/**
- * Created by manavdutta1 on 1/24/16.
- */
-public class Friend {
-}
